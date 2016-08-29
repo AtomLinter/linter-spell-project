6ea1151ff90b3680ca978aed7aabbe4064bc5956
Tarn Burton <twburton@gmail.com>
2016-08-29 12:04:54 -0400

    Update to linter-spell-word-list v0.4.0

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,33 +1,24 @@
 'use babel'
 
-// import * as path from 'path'
-// import * as _ from 'lodash'
-// import { Range } from 'atom'
+import * as _ from 'lodash'
+import * as path from 'path'
 
-describe('The linter-spell-project provider for Atom Linter', () => {
-  //const grammar = require('../lib/main').provideGrammar()[0]
+describe('Project dictionary for Atom Linter', () => {
+  const dictionary = require('../lib/main').provideDictionary()
 
-  // beforeEach(() => {
-  //   waitsForPromise(() => {
-  //     return atom.packages.activatePackage('linter-spell-project')
-  //   })
-  // })
+  beforeEach(() => {
+    waitsForPromise(() => {
+      return atom.packages.activatePackage('linter-spell-project')
+    })
+  })
 
-  // it('finds spelling regions in "foo.tex"', () => {
-  //   waitsForPromise(() => {
-  //     return atom.workspace.open(path.join(__dirname, 'files', 'foo.tex')).then(editor => {
-  //       expect(_.isEqual(
-  //         [new Range([2, 0], [3, 0]), new Range([4, 0], [0, 24])],
-  //         grammar.getRanges(editor, [editor.getBuffer().getRange()]).ignoredRanges)).toBe(true, 'Matching ranges')
-  //     })
-  //   })
-  // })
-
-  // it('finds language "foo.javascript"', () => {
-  //   waitsForPromise(() => {
-  //     return atom.workspace.open(path.join(__dirname, 'files', 'foo.javascript')).then(editor => {
-  //       expect(_.isEqual(grammar.getDictionaries(editor), ['en_US'])).toBe(true, 'en_US dictionary')
-  //     })
-  //   })
-  // })
+  it('finds a spelling in "foo.txt"', () => {
+    waitsForPromise(() => {
+      return atom.workspace.open(path.join(__dirname, 'files', 'foo.txt')).then(editor => {
+        return lint(editor).then(messages => {
+          expect(_.some(messages, (message) => { return message.text.match(/^armour( ->|$)/) })).toBe(true)
+        })
+      })
+    })
+  })
 })
