07ea07eb5f0c9672360aa665670fbae27176e0fb
Tarn Burton <twburton@gmail.com>
2016-09-03 08:42:40 -0400

    Add spec

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -2,22 +2,29 @@
 
 import * as _ from 'lodash'
 import * as path from 'path'
+import { Range } from 'atom'
 
 describe('Project dictionary for Atom Linter', () => {
-  const dictionary = require('../lib/main').provideDictionary()
-
   beforeEach(() => {
     waitsForPromise(() => {
+      atom.project.addPath('.')
       return atom.packages.activatePackage('linter-spell-project')
     })
   })
 
   it('finds a spelling in "foo.txt"', () => {
+    console.log(atom.project.getPaths())
     waitsForPromise(() => {
       return atom.workspace.open(path.join(__dirname, 'files', 'foo.txt')).then(editor => {
-        return lint(editor).then(messages => {
-          expect(_.some(messages, (message) => { return message.text.match(/^armour( ->|$)/) })).toBe(true)
-        })
+        let dictionary = require('../lib/main').provideDictionary()
+        return Promise.all([
+          dictionary.checkWord(editor, ['en-US'], new Range([0, 0], [0, 3])).then(result => {
+            expect(result.isWord).toBe(false, 'Verify that the word "one" is not in the project dictionary.')
+          }),
+          dictionary.checkWord(editor, ['en-US'], new Range([0, 4], [0, 11])).then(result => {
+            expect(result.isWord).toBe(true, 'Verify that the word "morning" is in the project dictionary.')
+          })
+        ])
       })
     })
   })
